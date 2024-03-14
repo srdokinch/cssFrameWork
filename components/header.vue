21e92bb20a1637c1b3d1c9cb562930d5083279e5
srdokinch <srdokinch@gmail.com>
2024-03-15 06:49:39 +0900

    tailwindインストール

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="header">
-    <h1 class="logo"> <a href="#"><img src="~/assets/logo.png" alt="SKY COFFEE"></a></h1>
+    <h1 class="logo"> <a href="#"><img src="~/assets/images/logo.png" alt="SKY COFFEE"></a></h1>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <Header />
+    <Header />
 
     <!-- main-visual -->
     <div class="main-visual">
       <picture>
-        <source media="(min-width: 768px)" srcset="~/assets/pc_main-visual.jpg">
-        <img src="~/assets/sp_main-visual.jpg" alt="複数の青いカップのコーヒー">
+        <source media="(min-width: 768px)" srcset="~/assets/images/pc_main-visual.jpg">
+        <img src="~/assets/images/sp_main-visual.jpg" alt="複数の青いカップのコーヒー">
       </picture>
     </div>
     <!-- /main-visual -->
@@ -18,7 +18,7 @@
       <section class="concept">
         <div class="container">
           <div class="img">
-            <img src="~/assets/concept.jpg" alt="青いカップのコーヒー" width="750" height="759">
+            <img src="~/assets/images/concept.jpg" alt="青いカップのコーヒー" width="750" height="759">
           </div>
           <div class="desc">
             <div class="subtitle">Concept</div>
@@ -41,42 +41,42 @@
           <div class="list">
             <div class="item">
               <a href="#">
-                <div class="img"><img src="~/assets/thumbnail01.jpg" alt="青いトラック" width="700" height="357" /></div>
+                <div class="img"><img src="~/assets/images/thumbnail01.jpg" alt="青いトラック" width="700" height="357" /></div>
                 <div class="title">コーヒーカー始めました。</div>
                 <div class="date">2030.08.08</div>
               </a>
             </div>
             <div class="item">
               <a href="#">
-                <div class="img"><img src="~/assets/thumbnail02.jpg" alt="牛乳" width="700" height="357" /></div>
+                <div class="img"><img src="~/assets/images/thumbnail02.jpg" alt="牛乳" width="700" height="357" /></div>
                 <div class="title">濃厚クリーミーなミルクをどうぞ</div>
                 <div class="date">2030.08.05</div>
               </a>
             </div>
             <div class="item">
               <a href="#">
-                <div class="img"><img src="~/assets/thumbnail03.jpg" alt="音楽アプリ" width="700" height="357" /></div>
+                <div class="img"><img src="~/assets/images/thumbnail03.jpg" alt="音楽アプリ" width="700" height="357" /></div>
                 <div class="title">Sky Coffeeのオリジナル曲が完成！無料ダウンロード可能です。</div>
                 <div class="date">2030.08.02</div>
               </a>
             </div>
             <div class="item">
               <a href="#">
-                <div class="img"><img src="~/assets/thumbnail04.jpg" alt="山々" width="700" height="357" /></div>
+                <div class="img"><img src="~/assets/images/thumbnail04.jpg" alt="山々" width="700" height="357" /></div>
                 <div class="title">ハイキング用のコーヒーを販売中です。</div>
                 <div class="date">2030.07.28</div>
               </a>
             </div>
             <div class="item">
               <a href="#">
-                <div class="img"><img src="~/assets/thumbnail05.jpg" alt="青とピンクのグラデーション" width="700" height="357" /></div>
+                <div class="img"><img src="~/assets/images/thumbnail05.jpg" alt="青とピンクのグラデーション" width="700" height="357" /></div>
                 <div class="title">ショップの壁を塗ってみました。</div>
                 <div class="date">2030.07.27</div>
               </a>
             </div>
             <div class="item">
               <a href="#">
-                <div class="img"><img src="~/assets/thumbnail06.jpg" alt="コーヒーと手帳" width="700" height="357" /></div>
+                <div class="img"><img src="~/assets/images/thumbnail06.jpg" alt="コーヒーと手帳" width="700" height="357" /></div>
                 <div class="title">ブログをはじめました。</div>
                 <div class="date">2030.07.26</div>
               </a>
