2eb436a16e0262a23c3fc5c502546dba7238546c
srdokinch <srdokinch@gmail.com>
2024-05-13 21:28:45 +0900

    とりあえず一旦コミット

---
+++ components/card.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="item md:w-1/3">
+    <a href="#">
+      <div class="img"><img src="~/assets/images/thumbnail02.jpg" alt="牛乳" width="700" height="357" /></div>
+      <div class="title">濃厚クリーミーなミルクをどうぞ</div>
+      <div class="date">2030.08.05</div>
+    </a>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -39,48 +39,7 @@
         <div class="container">
           <h2 class="heading">blog<span class="subtitle">ブログ</span></h2>
           <div class="list md:flex md:flex-wrap">
-            <div class="item md:w-1/3">
-              <a href="#">
-                <div class="img"><img src="~/assets/images/thumbnail01.jpg" alt="青いトラック" width="700" height="357" /></div>
-                <div class="title">コーヒーカー始めました。</div>
-                <div class="date">2030.08.08</div>
-              </a>
-            </div>
-            <div class="item md:w-1/3">
-              <a href="#">
-                <div class="img"><img src="~/assets/images/thumbnail02.jpg" alt="牛乳" width="700" height="357" /></div>
-                <div class="title">濃厚クリーミーなミルクをどうぞ</div>
-                <div class="date">2030.08.05</div>
-              </a>
-            </div>
-            <div class="item md:w-1/3">
-              <a href="#">
-                <div class="img"><img src="~/assets/images/thumbnail03.jpg" alt="音楽アプリ" width="700" height="357" /></div>
-                <div class="title">Sky Coffeeのオリジナル曲が完成！無料ダウンロード可能です。</div>
-                <div class="date">2030.08.02</div>
-              </a>
-            </div>
-            <div class="item md:w-1/3">
-              <a href="#">
-                <div class="img"><img src="~/assets/images/thumbnail04.jpg" alt="山々" width="700" height="357" /></div>
-                <div class="title">ハイキング用のコーヒーを販売中です。</div>
-                <div class="date">2030.07.28</div>
-              </a>
-            </div>
-            <div class="item">
-              <a href="#">
-                <div class="img"><img src="~/assets/images/thumbnail05.jpg" alt="青とピンクのグラデーション" width="700" height="357" /></div>
-                <div class="title">ショップの壁を塗ってみました。</div>
-                <div class="date">2030.07.27</div>
-              </a>
-            </div>
-            <div class="item">
-              <a href="#">
-                <div class="img"><img src="~/assets/images/thumbnail06.jpg" alt="コーヒーと手帳" width="700" height="357" /></div>
-                <div class="title">ブログをはじめました。</div>
-                <div class="date">2030.07.26</div>
-              </a>
-            </div>
+            <card />
           </div>
           <a href="#" class="btn">
             ブログ一覧
